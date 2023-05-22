0541f77ca1cea6a6d6db97d963d4f50419a005f2
Kai Klede <kai.klede@fau.de>
2023-05-22 13:52:59 +0200

    black formatting

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from Cython.Build import cythonize
 from numpy import get_include
 
 setup(
-    ext_modules=cythonize(["standardized_mutual_info.pyx"],
-                          language_level="3"),
-    include_dirs=[get_include()]
+    ext_modules=cythonize(["standardized_mutual_info.pyx"], language_level="3"),
+    include_dirs=[get_include()],
 )
